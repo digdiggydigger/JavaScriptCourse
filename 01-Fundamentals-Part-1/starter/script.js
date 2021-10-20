95c6dd2255dcb5bbce603b55cf188640675e7b34
Ethan Anthony <ethanant@icloud.com>
2021-10-20 21:08:58 +0100

    Section 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,33 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 10 - 23 + 100);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Joey"
+// let $first = "Joey"
+// let first_person = "Joey"
+
+
+// console.log(firstName);
+// console.log($first);
+// console.log(first_person);
+
+
+// let country = "United Kingdom";
+// let continent = "Africa";
+// let population = "100,770,949";
+
+// console.log(country, continent, population);
+
+
+////////////////////////////////////////////////
+
+const birthYear = 2000; //const will not allow the variables value to be changed
+console.log(birthYear);
+let birthday = 1999; //let will allow me to change the variable as wanted
+console.log(birthYear);
+
+const now = 2021;
+const ageEthan = 2021 - 2000;
+console.log(ageEthan * 2);
